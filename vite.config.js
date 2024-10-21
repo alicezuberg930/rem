98e482ac8473bce226fdbaefdb8d8abc7fd7df69
tien.nguyenvinh <tien.nguyenvinh@di4l.vn>
2024-10-21 16:25:32 +0700

    update vercel 5

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel(),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
     ],
 });
