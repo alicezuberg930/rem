c93eb2b4652244b1a71c625fe38f1d42363e3b3d
tien.nguyenvinh <tien.nguyenvinh@di4l.vn>
2024-10-21 19:52:45 +0700

    update vercel 8

--- resources/views/components/head_tag.blade.php
+++ resources/views/components/head_tag.blade.php
@@ -2,6 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}" />
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
 <script src="{{ url('jquery/dist/jquery.min.js') }}"></script>
 <script src="{{ url('popper/dist/umd/popper.min.js') }}"></script>
